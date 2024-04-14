1be221d753fbeb12148e759f9f93e92be645340d
Saifullah <saifullah.saifullah@dfki.uni-kl.de>
2024-04-14 19:07:23 +0200

    Added data prep for cifar10.

---
+++ scripts/run/prepare_datasets/prepare_cifar10.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+$SCRIPT_DIR/../../../scripts/analyze.sh -c prepare_datasets +run=prepare_image_dataset dataset_config_name=default args/data_args=image_classification/cifar10 visualize=true realtime_image_size_x=224 realtime_image_size_y=224
