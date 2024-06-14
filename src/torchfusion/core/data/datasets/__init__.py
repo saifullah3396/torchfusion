3c9bdb86e74331c559f40aac679adb97bd8e4bb2
saifullah <saifullah.saifullah@dfki.de>
2024-06-14 11:01:06 +0200

    This is turned off for now as adding dataset to registry imports all sorts of libraries which is not good for code startup.

--- src/torchfusion/core/data/datasets/__init__.py
+++ src/torchfusion/core/data/datasets/__init__.py
@@ -1,19 +1,19 @@
-import inspect
+# import inspect
 
-from datasets.packaged_modules import (
-    _EXTENSION_TO_MODULE,
-    _MODULE_SUPPORTS_METADATA,
-    _PACKAGED_DATASETS_MODULES,
-    _hash_python_lines,
-)
+# from datasets.packaged_modules import (
+#     _EXTENSION_TO_MODULE,
+#     _MODULE_SUPPORTS_METADATA,
+#     _PACKAGED_DATASETS_MODULES,
+#     _hash_python_lines,
+# )
 
-# here instead of adding it to our own registery, we add it to huggingface packaged_modules
-# this is necessary to properly handle data_files automatically from folder structure
-import torchfusion.core.data.datasets.fusion_image_folder_dataset as fusion_image_folder_dataset
+# # here instead of adding it to our own registery, we add it to huggingface packaged_modules
+# # this is necessary to properly handle data_files automatically from folder structure
+# import torchfusion.core.data.datasets.fusion_image_folder_dataset as fusion_image_folder_dataset
 
-_FILE = fusion_image_folder_dataset
-_PACKAGED_DATASETS_MODULES["fusion_image_folder"] = (_FILE.__name__, _hash_python_lines(inspect.getsource(_FILE)))
-_EXTENSION_TO_MODULE.update(
-    {ext[1:]: ("fusion_image_folder", {}) for ext in fusion_image_folder_dataset.FusionImageFolderDataset.EXTENSIONS}
-)
-_MODULE_SUPPORTS_METADATA.update("fusion_image_folder")
+# _FILE = fusion_image_folder_dataset
+# _PACKAGED_DATASETS_MODULES["fusion_image_folder"] = (_FILE.__name__, _hash_python_lines(inspect.getsource(_FILE)))
+# _EXTENSION_TO_MODULE.update(
+#     {ext[1:]: ("fusion_image_folder", {}) for ext in fusion_image_folder_dataset.FusionImageFolderDataset.EXTENSIONS}
+# )
+# _MODULE_SUPPORTS_METADATA.update("fusion_image_folder")
